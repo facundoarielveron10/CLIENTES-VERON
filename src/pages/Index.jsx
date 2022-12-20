a6a6fb12de5203eed594c930ccc5d3804d16b484
facundoarielveron10 <facundoarielveron10@gmail.com>
2022-12-19 21:40:30 -0300

    Avanzando en el Proyecto

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,5 +1,76 @@
+import { useLoaderData } from 'react-router-dom';
+import Cliente from '../components/Cliente';
+
+export function loader() {
+	const clientes = [
+		{
+			id: 1,
+			nombre: 'Juan',
+			telefono: 102013313,
+			email: 'juan@juan.com',
+			empresa: 'Codigo Con Juan',
+		},
+		{
+			id: 2,
+			nombre: 'Karen',
+			telefono: 138198313,
+			email: 'karen@juan.com',
+			empresa: 'Codigo Con Juan',
+		},
+		{
+			id: 3,
+			nombre: 'Josue',
+			telefono: 31983913,
+			email: 'josue@juan.com',
+			empresa: 'Codigo Con Juan',
+		},
+		{
+			id: 4,
+			nombre: 'Miguel',
+			telefono: 319381983,
+			email: 'miguel@juan.com',
+			empresa: 'Codigo Con Juan',
+		},
+		{
+			id: 5,
+			nombre: 'Pedro',
+			telefono: 1398198938,
+			email: 'pedro@juan.com',
+			empresa: 'Codigo Con Juan',
+		},
+	];
+	return clientes;
+}
+
 const Index = () => {
-	return <div>Clientes</div>;
+	// Datos del Loader
+	const clientes = useLoaderData();
+
+	return (
+		<>
+			<h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+			<p className="mt-3">Administra tus Clientes</p>
+
+			{clientes.length ? (
+				<table className="[&>tbody>*:nth-child(odd)]:bg-gray-100 [&>tbody>*:nth-child(even):bg-white w-full bg-white shadow mt-5 table-auto">
+					<thead className="bg-blue-800 text-white">
+						<tr>
+							<th className="p-2">Cliente</th>
+							<th className="p-2">Contacto</th>
+							<th className="p-2">Acciones</th>
+						</tr>
+					</thead>
+					<tbody>
+						{clientes.map(cliente => (
+							<Cliente cliente={cliente} key={cliente.id} />
+						))}
+					</tbody>
+				</table>
+			) : (
+				<p className="text-center mt-10">No Hay Clientes Aun</p>
+			)}
+		</>
+	);
 };
 
 export default Index;
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,5 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+
 const NuevoCliente = () => {
-	return <div>NuevoCliente</div>;
+	const navigate = useNavigate();
+
+	return (
+		<>
+			<h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+			<p className="mt-3">
+				Llena todos los campos para registrar un nuevo cliente
+			</p>
+
+			<div className="flex justify-end">
+				<button
+					onClick={() => navigate('/')}
+					className="bg-blue-800 text-white mt-5 px-3 py-1 font-bold uppercase"
+				>
+					Volver
+				</button>
+			</div>
+
+			<div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-18">
+				<p>Formulario Aqui</p>
+			</div>
+		</>
+	);
 };
 
 export default NuevoCliente;
